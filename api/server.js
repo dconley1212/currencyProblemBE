e185c791011e68126928646770da914b046aa7d7
David Conley <dconley1989@gmail.com>
2022-09-05 17:00:48 -0600

    added the cors library and made the post route to handle the post request from the front-end and made a route to handle the errors

--- api/server.js
+++ api/server.js
@@ -1,5 +1,20 @@
 const express = require("express");
+const cors = require("cors");
 
 const server = express();
+server.use(express.json());
+server.use(cors());
+
+server.post("/price/difference", (req, res, next) => {
+  console.log(req.body.textArea);
+});
+
+server.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    custom: "Something went wrong",
+    message: err.mesage,
+    stack: err.stack,
+  });
+});
 
 module.exports = server;
