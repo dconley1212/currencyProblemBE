e9e27df56b969b25b9b065d2eaf925975d7623ca
David Conley <dconley1989@gmail.com>
2022-09-06 16:55:00 -0600

    made a lot of changes some for dumb mistakes and others as I test the api to get the results I want but still not getting the result I want

--- api/server.js
+++ api/server.js
@@ -1,56 +1,64 @@
 const express = require("express");
 const cors = require("cors");
-const { request } = require("express");
 
 const server = express();
 server.use(express.json());
 server.use(cors());
 
 server.post("/price/difference", (req, res, next) => {
-  console.log(req.body.textArea);
+  const { file, textArea } = req.body;
 
-  if (request.body.testArea) {
-    const dataSets = {};
-    let count = req.body.textArea[0].parseInt();
-    dataSetCount = 1;
-    function separateData(textAreaArray, count) {
-      if (textAreaArray.length === 0 || count === 0) {
-        return;
-      }
-
-      const denominations = textAreaArray[0][0].parseInt();
-      const differentPrices = textAreaArray[0][1].parseInt();
-      const notesQuantityString = textAreaArray[1];
-      notesQuantityString += "1";
-      let counterNote = 0;
-      let counterDenomination = 0;
-      let counterPrice = 2;
-      let maxPrice = 0;
-      let minPrice = 0;
-
-      while (differentPrices !== 0) {
-        const value =
-          notesQuantityString[counterNote].parseInt() *
-          textAreaArray[counterPrice][counterDenomination].parseInt();
-        maxPrice = Math.max(maxPrice, value);
-        minPrice = Math.min(minPrice, value);
-        counterNote++;
-        counterDenomination++;
-        if (counterDenomination === denominations) {
-          counterNote = 0;
-          counterDenomination = 0;
-          counterPrice++;
-          differentPrices--;
-        }
-      }
-      dataSets[`Data Set ${dataSetCount}`] = maxPrice - minPrice;
-      dataSetCount++;
-      separateData(textAreaArray.slice(5), count--);
+  //   console.log(req.body.textArea);
+  const dataSets = {};
+  //   if (req.body.textArea.length > 0) {
+  let count = parseInt(textArea[0]);
+  dataSetCount = 1;
+  function separateData(textAreaArray, count) {
+    if (textAreaArray.length === 0 || count === 0) {
+      return;
     }
-    const firstSlice = req.body.area.slice(1);
+    // console.log(textAreaArray);
+    let denominationsAndPrices = textAreaArray[0];
+    // console.log(denominationsAndPrices);
+    let denominations = parseInt(denominationsAndPrices[0]);
+    // console.log(denominations);
+    let differentPrices = parseInt(denominationsAndPrices[2]);
+    console.log(differentPrices);
+    let notesQuantityString = textAreaArray[2];
+    notesQuantityString += "1";
+    // console.log(notesQuantityString);
+    let counterNote = 0;
+    let counterDenomination = 0;
+    let counterPrice = 2;
+    let maxPrice = 0;
+    let minPrice = 0;
 
-    separateData(firstSlice, count);
+    while (differentPrices !== 0) {
+      const value =
+        parseInt(notesQuantityString[counterNote]) *
+        parseInt(textAreaArray[counterPrice][counterDenomination]);
+      maxPrice = Math.max(maxPrice, value);
+      minPrice = Math.min(minPrice, value);
+      counterNote++;
+      counterDenomination++;
+      if (counterDenomination === denominations) {
+        counterNote = 0;
+        counterDenomination = 0;
+        counterPrice++;
+        // console.log(differentPrices);
+        differentPrices--;
+      }
+    }
+    dataSets[`Data Set ${dataSetCount}`] = maxPrice - minPrice;
+    dataSetCount++;
+    console.log(textAreaArray.slice(counterPrice + 1));
+    separateData(textAreaArray.slice(counterPrice), count--);
   }
+  let firstSlice = textArea.slice(1);
+  console.log(firstSlice);
+  separateData(firstSlice, count);
+  // }
+  // console.log(dataSets);
   res.status(200);
 });
 
