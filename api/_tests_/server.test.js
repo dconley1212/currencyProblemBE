143b33eed483103368ba4ab11f0984b64e9837f7
David Conley <dconley1989@gmail.com>
2022-09-07 14:21:59 -0600

    added on more test and added another post api

--- api/_tests_/server.test.js
+++ api/_tests_/server.test.js
@@ -1,9 +1,17 @@
 const server = require("../server");
 const request = require("supertest");
 
-describe("[POST] /price/difference tests", () => {
-  test("should return a 200 ok status"),
+//Added a couple of tests to see if I was getting a 200 response when pinging the api
+
+describe("[POST] tests", () => {
+  test("[POST] /textArea returns a 200 ok status"),
     async () => {
-      const response = await request(server).post("/price/difference");
+      const response = await request(server).post("/textArea");
+      expect(response).toBe(200);
+    };
+  test("[POST] /file returns a 200 ok status "),
+    async () => {
+      const response = await request(server).post("/file");
+      expect(response).toBe(200);
     };
 });
--- api/server.js
+++ api/server.js
@@ -1,39 +1,52 @@
 const express = require("express");
 const cors = require("cors");
+const fs = require("fs");
+const multer = require("multer");
+const upload = multer({ dest: "./public/data/uploads/" });
 
 const server = express();
 server.use(express.json());
 server.use(cors());
 
-server.post("/price/difference", (req, res, next) => {
-  const { file, textArea } = req.body;
+/* As you can probably see I might have overcomplicated my code and maybe that ultimately
+tripped me up. I tried to take the array full of strings from the text area and recursively
+take each dataSet. Then save the important values to variables and get the maxProfit 
+for each data set during each recursive call until it hit the end of the array.
+My plan didn't work because I kept running into unforeseen problems with the data in my array.
+For example the strings of numbers would have sparsed strings where the length was longer than
+the number of values. That tripped me up for a while. Anyways I didn't get it to work.
+*/
 
+server.post("/textArea", (req, res, next) => {
   //   console.log(req.body.textArea);
+  const { textArea } = req.body;
   const dataSets = {};
-  //   if (req.body.textArea.length > 0) {
+
   let count = parseInt(textArea[0]);
   dataSetCount = 1;
   function separateData(textAreaArray, count) {
     if (textAreaArray.length === 0 || count === 0) {
       return;
     }
+    console.log(textAreaArray.length);
     // console.log(textAreaArray);
     let denominationsAndPrices = textAreaArray[0];
     // console.log(denominationsAndPrices);
     let denominations = parseInt(denominationsAndPrices[0]);
     // console.log(denominations);
+
     let differentPrices = parseInt(denominationsAndPrices[2]);
     console.log(differentPrices);
-    let notesQuantityString = textAreaArray[2];
+    let notesQuantityString = textAreaArray[1];
     notesQuantityString += "1";
-    // console.log(notesQuantityString);
+    console.log(notesQuantityString);
     let counterNote = 0;
     let counterDenomination = 0;
     let counterPrice = 2;
     let maxPrice = 0;
     let minPrice = 0;
 
-    while (differentPrices !== 0) {
+    while (differentPrices > 0) {
       const value =
         parseInt(notesQuantityString[counterNote]) *
         parseInt(textAreaArray[counterPrice][counterDenomination]);
@@ -51,15 +64,20 @@ server.post("/price/difference", (req, res, next) => {
     }
     dataSets[`Data Set ${dataSetCount}`] = maxPrice - minPrice;
     dataSetCount++;
-    console.log(textAreaArray.slice(counterPrice + 1));
-    separateData(textAreaArray.slice(counterPrice), count--);
+    let nextSlice = parseInt(denominationsAndPrices[2]) + denominations;
+    console.log(textAreaArray.slice(nextSlice));
+    separateData(textAreaArray.slice(nextSlice), count--);
   }
   let firstSlice = textArea.slice(1);
   console.log(firstSlice);
   separateData(firstSlice, count);
-  // }
+
   // console.log(dataSets);
   res.status(200);
+});
+
+server.post("/file", upload.single("uploaded_file"), (req, res, next) => {
+  console.log(req.body);
 });
 
 server.use((err, req, res, next) => {
